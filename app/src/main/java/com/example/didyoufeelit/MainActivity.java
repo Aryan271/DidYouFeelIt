6742030af9a6dd01ced90af482b6cc3b4b3b3e61
Aryan <27aryanraj@gmail.com>
2020-06-28 23:04:33 +0530

    First commit

---
+++ app/src/main/java/com/example/didyoufeelit/MainActivity.java
@@ -0,0 +1,64 @@
+package com.example.didyoufeelit;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final String USGS_REQUEST_URL =
+            "https://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson&starttime=2016-01-01&endtime=2016-05-02&minfelt=50&minmagnitude=5";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        DownloadTask task = new DownloadTask();
+        task.execute(USGS_REQUEST_URL);
+
+    }
+
+    private void updateUi(Event earthquake) {
+
+        TextView titleTextView = (TextView) findViewById(R.id.title);
+        titleTextView.setText(earthquake.title);
+
+        TextView tsunamiTextView = (TextView) findViewById(R.id.number_of_people);
+        tsunamiTextView.setText(getString(R.string.num_people_felt_it, earthquake.numOfPeople));
+
+        TextView magnitudeTextView = (TextView) findViewById(R.id.perceived_magnitude);
+        magnitudeTextView.setText(earthquake.perceivedStrength);
+    }
+
+    private class DownloadTask extends AsyncTask<String,Void,Event>
+    {
+
+        @Override
+        protected Event doInBackground(String... urls) {
+           //invalid url to perform network request
+            if(urls.length < 1 || urls[0] == null)
+            {
+                return null;
+            }
+                // Perform the HTTP request for earthquake data and process the response
+                Event earthquake = Utils.fetchEarthquakeData(urls[0]);
+                return earthquake;
+
+        }
+
+        @Override
+        protected void onPostExecute(Event earthquake)
+        {
+            if(earthquake == null)
+            {
+                return;
+            }
+            // Update the information displayed to the user.
+            updateUi(earthquake);
+        }
+    }
+    }
+
